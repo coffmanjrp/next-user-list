6c23d42988e5bcadd345281a16d22db6ffdfefd1
coffmanjrp <coffmanjrp@gmail.com>
2021-03-15 14:52:15 -0700

    Create pages and routes

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, corrupti
+        quisquam ut quis nulla dignissimos commodi praesentium asperiores
+        consequatur, molestias a odit minima cum quasi iste doloribus temporibus
+        reprehenderit reiciendis!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, corrupti
+        quisquam ut quis nulla dignissimos commodi praesentium asperiores
+        consequatur, molestias a odit minima cum quasi iste doloribus temporibus
+        reprehenderit reiciendis!
+      </p>
+    </div>
+  );
+};
+
+export default About;
