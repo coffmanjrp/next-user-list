7caab1156044d1cdf7b1b0c4b18afe63df947198
coffmanjrp <coffmanjrp@gmail.com>
2021-03-15 18:05:11 -0700

    Fetch data and show from list page

--- pages/list/index.jsx
+++ pages/list/index.jsx
@@ -1,7 +1,28 @@
-const Index = () => {
+import styles from '../../styles/List.module.css';
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
+
+const Index = ({ users }) => {
   return (
     <div>
       <h1>List</h1>
+      {users &&
+        users.map((user) => (
+          <div key={user.id}>
+            <a className={styles.single}>
+              <h3>{user.name}</h3>
+            </a>
+          </div>
+        ))}
     </div>
   );
 };
